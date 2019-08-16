5da69657ac00cf3757f92e04ccdc83785e2dbf22
Gerard Hynes <gkhynes@gmail.com>
2019-08-16 20:43:19 +0100

    Make ColorBox responsive

--- src/styles/PaletteStyles.js
+++ src/styles/PaletteStyles.js
@@ -1,3 +1,4 @@
+import sizes from "./sizes";
 export default {
   Palette: {
     height: "100vh",
@@ -36,6 +37,18 @@ export default {
       border: "none",
       cursor: "pointer",
       textDecoration: "none"
+    },
+    [sizes.down("lg")]: {
+      width: "25%",
+      height: "33.333%"
+    },
+    [sizes.down("md")]: {
+      width: "50%",
+      height: props => (props.showingFullPalette ? "10%" : "20%")
+    },
+    [sizes.down("xs")]: {
+      width: "100%",
+      height: "10%"
     }
   }
 };
